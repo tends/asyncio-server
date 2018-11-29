fa042ba8bb08bc4f51bdf920ddb959eab11b2abf
dimas <dimas@local>
2018-11-29 19:32:18 +0300

    Fix problems main.py

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3.7
 
 import asyncio
+from inspect import getmembers, isfunction
+import solvers
+
+solvers_dict = {o[0]: o[1] for o in getmembers(solvers) if isfunction(o[1])}
 
 
 async def handle_echo(reader, writer):
     while not reader.at_eof():
         data = await reader.readline()
         try:
-            message = data.decode()
+            parsed_msg = data.decode().split()
         except Exception as e:
             message = "Error {}".format(e)
+        else:
+            print(parsed_msg)
+            message = await solvers_dict[parsed_msg[0]](parsed_msg[1:]) \
+                if len(parsed_msg) >= 1 \
+                and parsed_msg[0] in solvers_dict \
+                else "E wrong parsed command {}".format(parsed_msg)
 
         addr = writer.get_extra_info('peername')
 
         print(f"Received {message!r} from {addr!r}")
 
         print(f"Send: {message!r}")
-        writer.write(data)
+        writer.write((str(message) + "\n").encode('utf-8'))#data)
         await writer.drain()
 
     print("Close the connection")
